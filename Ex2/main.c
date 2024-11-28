b06230256fcdfd774731a4de35ddb554fb54eb86
yangxin017 <yangzin8002@gmail.com>
2024-11-28 13:46:37 +0800

    Update main.c

--- Ex2/main.c
+++ Ex2/main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main(){
+  printf("   November 2024/n")
+  printf("Mo Tu We Th Fr Sa Su/n")
+  printf("                1  2/n")
+  printf(" 3  4  5  6  7  8  9/n")
+  printf("10 11 12 13 14 15 16/n")
+  printf("17 18 19 20 21 22 23/n")
+  printf("24 25 26 27 28 29 30/n")
 
+}
